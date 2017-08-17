4dba272681f9a6d758b292b3ed8e02c19e66d60d
JaredTan <jared.e.tan@gmail.com>
2017-08-17 11:15:39 -0700

    kth highest in k + (n-k)logk

--- jared_tan_algo_3/lib/k_largest_elements.rb
+++ jared_tan_algo_3/lib/k_largest_elements.rb
@@ -1,13 +1,19 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-  (2..array.length - 1).each do |i|
-    BinaryMinHeap.heapify_up(array, i - 1, i)
-  end
-
-  (2..array.length - 1).each do |i|
-    array[array.length - i], array[0] = array[0], array[array.length - i]
-    BinaryMinHeap.heapify_down(array, 0, array.length - i)
-  end
-  array.take(k)
+  heap = BinaryMinHeap.new
+  array.each do |el|
+    heap.push(el)
+    heap.extract if heap.count > k
+    end
+  heap.store
+  # (2..array.length - 1).each do |i|
+  #   BinaryMinHeap.heapify_up(array, i - 1, i)
+  # end
+  #
+  # (2..array.length - 1).each do |i|
+  #   array[array.length - i], array[0] = array[0], array[array.length - i]
+  #   BinaryMinHeap.heapify_down(array, 0, array.length - i)
+  # end
+  # array.take(k)
 end
