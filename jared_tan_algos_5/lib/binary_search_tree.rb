150c660977529af75af72b900e95c315851cb7d3
JaredTan <jared.e.tan@gmail.com>
2017-08-19 12:57:44 -0700

    bst delete and max

--- jared_tan_algos_5/lib/binary_search_tree.rb
+++ jared_tan_algos_5/lib/binary_search_tree.rb
@@ -10,18 +10,18 @@ class BinarySearchTree
   end
 
   def insert(value)
-    return @root = BSTNode.new(value) if !@root
+    return @root = BSTNode.new(value, nil) if !@root
     current_node = @root
     while true
       if value <= current_node.value
         if !current_node.left
-          return current_node.left = BSTNode.new(value)
+          return current_node.left = BSTNode.new(value, current_node)
         else
           current_node = current_node.left
         end
       else
         if !current_node.right
-          return current_node.right = BSTNode.new(value)
+          return current_node.right = BSTNode.new(value, current_node)
         else
           current_node = current_node.right
         end
@@ -30,8 +30,8 @@ class BinarySearchTree
   end
 
   def find(value, tree_node = @root)
-    return nil if !@root
-    current_node = @root
+    return nil if !tree_node
+    current_node = tree_node
     while current_node
       if value == current_node.value
         return current_node
@@ -45,10 +45,74 @@ class BinarySearchTree
   end
 
   def delete(value)
+    node = find(value)
+    if !node
+      return nil
+    end
+
+    if !node.left && !node.right
+      if node.value == @root.value
+        return @root = nil
+      end
+      if node.value > node.parent.value
+        node.parent.right = nil
+        node = nil
+      else
+        node.parent.left = nil
+        node = nil
+      end
+    elsif !node.left && node.right
+      if node.value > node.parent.value
+        node.parent.right = node.right
+      else
+        node.parent.left = node.right
+      end
+    elsif !node.right && node.left
+      if node.value > node.parent.value
+        node.parent.right = node.left
+      else
+        node.parent.left = node.left
+      end
+    elsif node.left && node.right
+      replacement_node = maximum(node.left)
+      old_left_node = node.left
+      old_right_node = node.right
+      if node.value == @root.value
+        @root = replacement_node
+        if !replacement_node.left
+          replacement_node.parent.right = nil
+        else
+          replacement_node.parent.right = replacement_node.left
+        end
+        @root.parent = nil
+        @root.left = old_left_node
+        @root.right = old_right_node
+        old_left_node.parent = @root
+        old_right_node.parent = @root
+      else
+        if node.value > node.parent.value
+          node.parent.right = replacement_node
+        else
+          node.parent.left = replacement_node
+        end
+        replacement_node.left ? replacement_node.parent.right = replacement_node.left : replacement_node.parent.right = nil
+        replacement_node.parent = node.parent
+        replacement_node.value == old_left_node.value ? replacement_node.left = nil : replacement_node.left = old_left_node
+        replacement_node.value == old_right_node.value ? replacement_node.right = nil : replacement_node.right = old_right_node
+      end
+      node = nil
+    end
+
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    current_node = tree_node
+    while current_node
+      return current_node if !current_node.right
+      current_node = current_node.right
+    end
+    return nil
   end
 
   def depth(tree_node = @root)
--- jared_tan_algos_5/lib/bst_node.rb
+++ jared_tan_algos_5/lib/bst_node.rb
@@ -1,8 +1,10 @@
 class BSTNode
-  attr_accessor :value, :left, :right
-  def initialize(value)
+  attr_reader :value
+  attr_accessor :left, :right, :parent
+  def initialize(value, parent = nil)
     @value = value
     @left = nil
     @right = nil
+    @parent = parent
   end
 end
