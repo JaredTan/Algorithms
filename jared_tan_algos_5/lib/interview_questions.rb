28e6bf182f8dd1e9a37d29ddb4e170644cfcd763
JaredTan <jared.e.tan@gmail.com>
2017-08-22 00:32:57 -0700

    in order post order pre order traversals

--- jared_tan_algos_5/lib/interview_questions.rb
+++ jared_tan_algos_5/lib/interview_questions.rb
@@ -3,19 +3,20 @@
 require_relative 'binary_search_tree'
 
   def post_order_traversal(tree_node, arr = [] )
-    arr << post_order_traversal(tree_node.left) if tree_node.left
-    tree_node
-    arr << post_order_traversal(tree_node.right) if tree_node.right
+    post_order_traversal(tree_node.left, arr) if tree_node.left
+    post_order_traversal(tree_node.right, arr) if tree_node.right
+    arr << tree_node
+    arr
   end
-
 
 # Pre Order Traversal:
 
-def pre_order_traversal(tree_node, arr = [])
-  tree_node
-  arr << post_order_traversal(tree_node.left) if tree_node.left
-  arr << post_order_traversal(tree_node.right) if tree_node.right
-end
+  def pre_order_traversal(tree_node, arr = [])
+    arr << tree_node
+    pre_order_traversal(tree_node.left, arr) if tree_node.left
+    pre_order_traversal(tree_node.right, arr) if tree_node.right
+    arr
+  end
 
 
 # LCA:
@@ -27,20 +28,20 @@ end
 # function that takes in a BST and two nodes, and returns the node that is the
 # lowest common ancestor of the given nodes. Assume no duplicate values.
 
-def ancestor(bst, node1, node2)
+def ancestor(tree_node, node1, node2)
   node1.value < node2.value ?
   low_node = node1
   high_node = node2 :
   high_node = node1
   low_node = node2
-  return nil if bst.nil?
+  return nil if tree_node.nil?
   if bst.root.value.between?(low_node.value, high_node.value)
-    return bst.root
+    return tree_node
   elsif
     bst.root.value < low_node.value
-    ancestor(bst.right, node1, node2)
+    ancestor(tree_node.right, node1, node2)
   else
-    ancestor(bst.left, node1, node2)
+    ancestor(tree_node.left, node1, node2)
 end
 
 
