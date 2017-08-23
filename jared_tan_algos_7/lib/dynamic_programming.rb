01e0a12b1ab44caf1c4efd0a2ff452507f1601f7
JaredTan <jared.e.tan@gmail.com>
2017-08-22 23:13:49 -0700

    super frogs

--- jared_tan_algos_7/lib/dynamic_programming.rb
+++ jared_tan_algos_7/lib/dynamic_programming.rb
@@ -2,6 +2,9 @@ class DynamicProgramming
 
   def initialize
     @cache = {1=> 1, 2=> 2}
+    @frog_cache = {1=>[[1]], 2=>[[1, 1], [2]], 3 => [[1, 1, 1], [1, 2], [2, 1], [3]] }
+    @super_cache = { 1 => [[1]] }
+
   end
 
   def blair_nums(n)
@@ -40,16 +43,77 @@ class DynamicProgramming
   end
 
   def frog_hops_top_down(n)
-
+    return @frog_cache[n] if @frog_cache[n]
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
-
+    new_hops = []
+    frog_hops_top_down(n - 3).each do |hops|
+      @frog_cache[3].each do |base_hops|
+        new_hops << hops + base_hops unless new_hops.include?(hops + base_hops)
+      end
+    end
+    frog_hops_top_down(n - 2).each do |hops|
+      @frog_cache[2].each do |base_hops|
+        new_hops << hops + base_hops unless new_hops.include?(hops + base_hops)
+      end
+    end
+    frog_hops_top_down(n - 1).each do |hops|
+      @frog_cache[1].each do |base_hops|
+        new_hops << hops + base_hops unless new_hops.include?(hops + base_hops)
+      end
+    end
+    @frog_cache[n] = new_hops
   end
 
   def super_frog_hops(n, k)
+    super_cache = { 1 => [[1]]}
+    return super_cache[n] if super_cache[n]
+      if k > n
+        (2..n).each do |num|
+          new_hops = []
+          (1..num - 1).each do |num2|
+            super_cache[num2].each do |hops|
+              super_cache[num - num2].each do |base_hops|
+                new_hops << hops + base_hops unless new_hops.include?(hops + base_hops)
+              end
+            end
+          end
+          super_cache[num] = new_hops + [[num]]
+        end
+        p super_cache
+        return super_cache[n]
+      else
+        (2..k).each do |num|
+          new_hops = []
+          (1..num - 1).each do |num2|
+            super_cache[num2].each do |hops|
+              super_cache[num - num2].each do |base_hops|
+                new_hops << hops + base_hops unless new_hops.include?(hops + base_hops)
+              end
+            end
+          end
+          super_cache[num] = new_hops + [[num]]
+        end
+        ((k+1)..n).each do |num3|
+          new_hops = []
+          (1..k).each do |num4|
+            super_cache[num3 - num4].each do |hops|
+              super_cache[num4].each do |base_hops|
+                new_hops << hops + base_hops unless new_hops.include?(hops + base_hops)
+              end
+            end
+          end
+          super_cache[num3] = new_hops
+        end
+      end
+        # max_steps (k) < num_stairs (n)
+
+      super_cache[n]
 
   end
+
 
   def knapsack(weights, values, capacity)
 
